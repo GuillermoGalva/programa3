7dd174e0eeb9b382f1eead52adcb4e14916e47f4
guill <guill@GUILLERMO-PC>
2024-05-02 21:09:51 -0400

    Add project files.

---
+++ Class/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace programa3.Class
+{
+    class Calculadora
+    {
+        public static double CalcularSuma(double[] numeros)
+        {
+            double suma = 0;
+
+            foreach (double numero in numeros)
+            {
+                suma += numero;
+            }
+
+            return suma;
+        }
+
+        public static double CalcularPromedio(double suma)
+        {
+            return suma / 4;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using programa3.Class;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numeros = new double[4];
+
+        try
+        {
+            
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Ingrese el número {i + 1}:");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            
+            double suma = Calculadora.CalcularSuma(numeros);
+
+            
+            double promedio = Calculadora.CalcularPromedio(suma);
+
+          
+            Console.WriteLine($"La suma de los cuatro números es: {suma}");
+            Console.WriteLine($"El promedio de los cuatro números es: {promedio}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Por favor ingrese un número válido.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: El número ingresado es demasiado grande.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
